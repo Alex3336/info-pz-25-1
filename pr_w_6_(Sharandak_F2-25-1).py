10b194c5aa4f254c5f41ea0f93cafe0096d0d655
alex3336 <alexaamogus@gmail.com>
2026-05-25 15:39:55 +0300

    pr_w_6

--- pr_w_6_(Sharandak_F2-25-1).py
+++ pr_w_6_(Sharandak_F2-25-1).py
@@ -2,7 +2,7 @@ name = "Олександр"
 surname = "Шарандак"
 group = "ПЗ-25-1"
 
-module = __import__("pr_w_6.02_Sharandak_F2-25-1")
+module = __import__("pr_w_6_02_Sharandak_F2-25-1")
 Factorial = module.Factorial
 Allocation = module.Allocation
 Combinations = module.Combinations
@@ -10,10 +10,11 @@ Combinations = module.Combinations
 print(f"{name} {surname} \t {group} \n")
 
 try:
+    n = int(input("Укажіть загальну кількість елементів (n): "))
+    
     print("Чи враховується порядок наступності у сполуці?\n 1 - так\n 2 - ні")
     question_1 = int(input("Оберіть варіант відповіді: "))
 
-    n = int(input("Укажіть загальну кількість елементів (n): "))
 
     if question_1 == 2:
         k = int(input("Укажіть кількість елементів, які використовуються (k): "))
